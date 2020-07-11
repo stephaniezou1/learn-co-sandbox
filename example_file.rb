606c0036f9363a6e61e836069702203a7f956aba
Stephanie Zou <stephanie.zou3@gmail.com>
2020-07-11 19:54:07 +0000

    Automatically backed up by Learn

---
+++ example_file.rb
@@ -0,0 +1,7 @@
+spice_rack = [
+  ["Mace", "Ginger", "Marojam"],          
+  ["Paprika", "Fajita Mix", "Coriander"], 
+  ["Parsley", "Sage", "Rosemary"]         
+]
+
+spice_rack[1][1] = "Extract of Pizza"
